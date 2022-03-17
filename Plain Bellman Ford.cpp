380c8939923cc27e676cfa1bf8f319c33dbf661f
Sirly <45280500+SirlyDreamer@users.noreply.github.com>
2022-03-17 11:48:39 +0800

    Paste

---
+++ Plain Bellman Ford.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+using ll = long long;
+constexpr int maxn = 1e6 + 5;
+
+struct edge
+{
+	ll v, w;
+} tmp;
+
+vector<edge> mp[maxn];
+ll dis[maxn];
+ll vis[maxn];
+
+bool Bellman_Ford(int cur, ll n)
+{
+	memset(dis, 0x3f, sizeof dis);
+	bool ok(true);
+	dis[cur] = 0;
+
+	int lim(n);
+	while (lim-- && ok)
+	{
+		ok = false;
+		for (int u(1); u <= n; ++u)
+		{
+			for (auto& [v,w] : mp[u])
+			{
+				if (dis[v] > dis[u] + w)
+				{
+					dis[v] = dis[u] + w;
+					ok = true;
+				}
+			}
+		}
+	}
+	return ok;
+}
+
+int main()
+{
+	ll m, n, s, t;
+	cin >> n >> m >> s >> t;
+	for (int i(0); i < m; ++i)
+	{
+		ll u, v, w;
+		cin >> u >> v >> w;
+		tmp.w = w;
+		tmp.v = v;
+		mp[u].push_back(tmp);
+		tmp.v = u;
+		mp[v].push_back(tmp);
+	}
+	Bellman_Ford(s,n);
+	cout << dis[t];
+	return 0;
+}
