b55dc4fd095b20f30acbdc980cbdb8be47010796
Sirly <45280500+SirlyDreamer@users.noreply.github.com>
2022-03-19 12:43:56 +0800

    Update

--- Floyd.cpp
+++ Floyd.cpp
@@ -1,31 +1,42 @@
 #include <iostream>
 #include <vector>
-#include <cstring>
 #include <queue>
+#include <cstring>
 using namespace std;
 using ll = long long;
 
 constexpr int maxn = 2505;
+constexpr ll inf = 0x3f3f3f3f3f3f3f3fL;
 
-ll dis[maxn][maxn] = { 0 };
+ll dis[maxn][maxn];
 
 int main() {
-    ll m, n, s, t;
+    int n, m, t;
+    cin >> n >> m >> t;
     memset(dis, 0x3f, sizeof dis);
-
-    cin >> n >> m >> s >> t;
+    for (int i(0); i <= n; ++i) 
+        dis[i][i] = 0;					//初始化，自己到自己的最短路为0
 
     for (int i(0); i < m; ++i) {
-        ll u, v, w;
+        int u, v;
+        ll w;
         cin >> u >> v >> w;
-        dis[u][v] = w;
-        dis[v][u] = w;
+        dis[u][v] = dis[v][u] = w;		//这里我们偷懒不存边，我们直接把u和v之间的最短路更新成其边权
+        								//如果有重边的话这里需要取其最小值
     }
 
-    for (int k(1); k <= n; ++k)
+    for (int k(1); k <= n; ++k)				//k要写在最外层！！！！
         for (int i(1); i <= n; ++i)
-            for (int j(1); j <= n; ++j) dis[i][j] = min(dis[i][j], dis[i][k] + dis[k][j]);
+            for (int j(1); j <= n; ++j) 
+                dis[i][j] = min(dis[i][j], dis[i][k] + dis[k][j]);	//状态转移
 
-    cout << dis[s][t];
+    for (int i(0); i < t; ++i) {
+        int u, v;
+        cin >> u >> v;
+        if (dis[u][v] == inf)
+            cout << "-1\n";
+        else
+            cout << dis[u][v] << "\n";
+    }
     return 0;
 }
