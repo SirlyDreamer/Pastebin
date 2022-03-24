442e59ef274508bd7f47afb4bc32cde058e32f0b
Sirly <45280500+SirlyDreamer@users.noreply.github.com>
2022-03-24 10:13:26 +0800

    Update

--- OverloadHash.cpp
+++ OverloadHash.cpp
@@ -12,7 +12,7 @@ namespace std
 			long long o;
 			for (auto& c : str)
 			{
-				o = c;
+				o += c;
 				o += 0x9e3779b97f4a7c15;
 				o = (o ^ (o >> 30)) * 0xbf58476d1ce4e5b9;
 				o = (o ^ (o >> 27)) * 0x94d049bb133111eb;
