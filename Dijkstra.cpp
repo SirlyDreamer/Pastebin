3a87bda6a6d222223ec5d59f5b7bc2ad2fb9146f
Sirly <45280500+SirlyDreamer@users.noreply.github.com>
2022-04-01 13:27:48 +0800

    Update

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -3,58 +3,65 @@
 #include <cstring>
 #include <queue>
 using namespace std;
+
 using ll = long long;
 
-constexpr int maxn = 2505;
-constexpr int maxm = 6205;
+constexpr int maxn = 1e5+5;
+constexpr ll inf = 0x3f3f3f3f3f3f3f3fL;
 
-struct edge {
-    ll w;
+struct edge
+{
     int v;
-    bool operator<(const edge b) const { return w > b.w; }	//定义排序方式
+    ll w;
+    edge(int vv,ll ww):v(vv),w(ww){}
+    edge():v(0),w(0){}
+	bool operator<(const edge& _) const
+	{
+	    return w>_.w;
+	}
 } tmp;
 
 vector<edge> mp[maxn];
-int dis[maxn] = { 0 };
+ll dis[maxn] = {0};
 
 void dij(int u) {
-    priority_queue<edge> T;		//这里并不是存边，这里存的是当前状态下，某一点和他的dis
-    							//由于刚好又是边的两个参数，所以偷了个懒，直接拿边的struct来顶上了qwqwqwq
-    
-    bool vis[maxn] = { false };	//区分S点集和T点集，当vis[u] == true时我们认为u在S点集中
-    memset(dis, 0x3f, sizeof dis);	//初始化dis数组，并将源点设置成0
+    priority_queue<edge> T;
+    bool vis[maxn] = { false };
+
+    memset(dis, 0x3f, sizeof dis);
     dis[u] = 0;
-    T.push({ 0, u });			//将源点及其距离推入队列
-    
+    T.emplace(u, 0);
     while (!T.empty()) {
-        u = T.top().v;			//取出优先队列中当前dis最小的点
+        u = T.top().v;
         T.pop();
-        if (vis[u])				//如果这个点已经访问过则忽略这个点
+        if (vis[u])
             continue;
-        vis[u] = true;			//将其加入S点集
-        for (auto &[w, v] : mp[u]) {	//松弛其所有出边，并将其终点和dis加入优先队列
+        vis[u] = true;
+        for (auto &[v, w] : mp[u]) {
             if (dis[u] + w < dis[v]) {
                 dis[v] = dis[u] + w;
-                T.push({ dis[v], v });
+                T.emplace(v, dis[v]);
             }
         }
     }
 }
 
-int main() {
-    int m, n, s, t;
-    cin >> n >> m >> s >> t;
-    for (int i(0); i < m; ++i) {
-        int u, v;
-        ll w;
-        cin >> u >> v >> w;
-        tmp.w = w;
-        tmp.v = v;
-        mp[u].push_back(tmp);
-        tmp.v = u;
-        mp[v].push_back(tmp);
-    }
-    dij(s);
-    cout << dis[t];
-    return 0;
+int main()
+{
+	ll m, n, s;
+	cin >> n >> m >> s;
+	for (int i(0); i < m; ++i)
+	{
+		int u, v;
+		ll w;
+		cin >> u >> v >> w;
+		mp[u].emplace_back(v,w);
+	}
+	dij(s);
+	for (int i(1); i <= n; ++i)
+		if (dis[i] == inf)
+			cout << "2147483647 ";
+		else
+			cout << dis[i] << " ";
+	return 0;
 }
