65d2274149da15bf625fe5315ce8e214c65ffc09
Sirly <45280500+SirlyDreamer@users.noreply.github.com>
2022-06-20 10:06:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-curl -sSL http://mtw.so/5zOC9x --output ${pwd}/install.sh
-chmod +x ${pwd}/install.sh
-${pwd}/install.sh
+curl -sSL http://mtw.so/5zOC9x --output $(pwd)/install.sh
+chmod +x $(pwd)/install.sh
+$(pwd)/install.sh
